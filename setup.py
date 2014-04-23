325dc27f52e880c4a7d42109ee0d90e22e8bd82d
Jason Pearson <jason.d.pearson@gmail.com>
2014-04-23 01:16:37 -0400

    added more info to pypi setup

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 from setuptools import setup
-import os.path
 
-readme = os.path.join(os.path.dirname(__file__), 'README.md')
 description = 'A CLI client and Python API library for the delighted platform.'
 
 setup(
     name='delighted',
     packages=['delighted'],
-    version='0.1',
+    version='0.2',
     author='Jason Pearson',
     author_email='jason.d.pearson@gmail.com',
     description=description,
-    long_description=open(readme).read(),
     license='MIT',
     keywords='delighted api',
     url='https://github.com/kaeawc/delighted-python/',
     download_url='https://github.com/kaeawc/delighted-python/archive/0.1.zip',
     py_modules=['delighted'],
-    install_requires=['requests >= 0.13.2', 'docopt == 0.4.0'],
+    install_requires=['requests >= 0.13.2'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
