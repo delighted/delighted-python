3a1f697266b6e37508661894b92ccb6d4a8328e8
Ben Turner <ben@delighted.com>
2018-05-22 17:08:11 -0700

    Version bump for GM release

--- delighted/__init__.py
+++ delighted/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'delighted'
-__version__ = '2.3.0rc1'
+__version__ = '2.3.0'
 __author__ = 'Ben Turner'
 __license__ = 'MIT'
 
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     test_requires=['mock', 'pytz', 'tzlocal'],
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
